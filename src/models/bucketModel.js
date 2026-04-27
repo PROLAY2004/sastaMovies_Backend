7f53f8041f9fd162377a02a3015dd692e547af7b
Prolay Halder <prolayhalder2004@gmail.com>
2026-04-27 20:41:44 +0530

    feat : bucket model created

---
+++ src/models/bucketModel.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const bucketSchema = new mongoose.Schema(
+  {
+    imdbUrl: {
+      type: String,
+      required: true,
+    },
+
+    baseUrl: {
+      type: String,
+      required: true,
+    },
+
+    chunkCount: {
+      type: Number,
+      required: true,
+    },
+
+    size_kb: {
+      type: Number,
+      required: true,
+    },
+
+    mimeType: {
+      type: String,
+      required: true,
+      enum: ['mp4', 'mkv', 'webm', 'mov', 'avi'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const bucket = mongoose.model('bucket', bucketSchema);
+export default bucket;
