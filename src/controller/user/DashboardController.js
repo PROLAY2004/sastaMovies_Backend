9a7c6420abc37554efd87bbe69e293c564473b52
Prolay Halder <prolayhalder2004@gmail.com>
2026-05-12 20:31:39 +0530

    Feat : dashboard controller modified

--- src/controller/user/DashboardController.js
+++ src/controller/user/DashboardController.js
@@ -1,14 +1,20 @@
 import DateFormatter from '../../utils/DateFormatter.js';
+import content from '../../models/contentModel.js';
 
 const format = new DateFormatter();
 
 export default class DashboardController {
   fetchProfile = async (req, res, next) => {
     try {
-      const userInfo = req.user;
+      const userInfo = req.user.toObject();
       const userSince = format.dateTemplate(userInfo.createdAt);
       const validTill = format.dateTemplate(userInfo.validTill);
       const contentCount = userInfo.savedContents.length;
+
+      userInfo.contentObjects = await content.find({
+        _id: { $in: userInfo.savedContents },
+        isDeleted: false,
+      });
 
       res.status(200).json({
         message: 'User Deails fetched successfully',
